60f93dfcb55256663237249fdf817cc480485719
Jose Antonio <llytocolegio123@gmail.com>
2019-12-16 10:31:41 +0100

    Profesor hacia cursos

--- F_acceso.php
+++ F_acceso.php
@@ -7,11 +7,26 @@
     </head>
     </html>
     <body>
+        <?php
+            session_start();
+            if (isset($_POST['submit'])) {
+                $usuario=$_POST['usuario'];
+                $clave=$_POST['contraseña'];
+                $_SESSION= $_POST['usuario'];
+                $_SESSION=$_POST['contraseña'];
+                if ($usuario="profesor") {
+                    header("Location: ./form_cursos.html");
+                }
+                elseif ($usuario="administrador") {
+                    header("Location: ./registro_aulas.html");
+                }
+            }
+        ?>
         <h1>Formulario de Registro</h1>
-        <form action="F_acceso.html" method="POST">
+        <form method="POST">
             <p>Nombre de usuario: <input type="text" name="usuario"></p>
             <p>Clave: <input type="password" name="contraseña" ></p>
-            <input type="submit" value="Acceder">
+            <input type="submit" value="Acceder" name="submit">
             <a href=".\index.html"><input type="button" value="Atras"></a>
         </form>
     </body>
